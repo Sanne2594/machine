a6fe381135ac8cd216fd51e6419a868e9c93ec22
Sanne Bouwmeester <sanne.wopwop@gmail.com>
2018-01-31 14:18:28 +0100

    additional test which prints line by line output and expected output

--- babitest.py
+++ babitest.py
@@ -8,11 +8,11 @@ import torchtext
 import seq2seq
 from seq2seq.loss import Perplexity
 from seq2seq.dataset import SourceField, TargetField
-from seq2seq.evaluator import Evaluator, Predictor
+from seq2seq.evaluator import Predictor
 from seq2seq.util.checkpoint import Checkpoint
 
 try:
-    raw_input          # Python 2
+    raw_input  # Python 2
 except NameError:
     raw_input = input  # Python 3
 
@@ -27,8 +27,8 @@ parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
 opt = parser.parse_args()
 
 if torch.cuda.is_available():
-        print("Cuda device set to %i" % opt.cuda_device)
-        torch.cuda.set_device(opt.cuda_device)
+    print("Cuda device set to %i" % opt.cuda_device)
+    torch.cuda.set_device(opt.cuda_device)
 
 #################################################################################
 # load model
@@ -40,36 +40,34 @@ input_vocab = checkpoint.input_vocab
 output_vocab = checkpoint.output_vocab
 
 ############################################################################
-# Prepare dataset and loss
-src = SourceField()
-tgt = TargetField()
-src.vocab = input_vocab
-tgt.vocab = output_vocab
-max_len = opt.max_len
+# # Prepare dataset to get vocabs
+# src = SourceField()
+# tgt = TargetField()
+# src.vocab = input_vocab
+# tgt.vocab = output_vocab
+# max_len = opt.max_len
+#
+#
+# def len_filter(example):
+#     return len(example.src) <= max_len and len(example.tgt) <= max_len
+#
+#
+# # generate test set: Still do this for vocabs
+# test = torchtext.data.TabularDataset(
+#     path=opt.test_data, format='tsv',
+#     fields=[('src', src), ('tgt', tgt)],
+#     filter_pred=len_filter
+# )
 
-def len_filter(example):
-    return len(example.src) <= max_len and len(example.tgt) <= max_len
+###########################################
+# Make some predictions
+f= open (opt.test_data)
+lines = f.readlines()
+f.close()
 
-# generate test set
-test = torchtext.data.TabularDataset(
-    path=opt.test_data, format='tsv',
-    fields=[('src', src), ('tgt', tgt)],
-    filter_pred=len_filter
-)
+predictor = Predictor(seq2seq, input_vocab, output_vocab)
 
-# Prepare loss
-#TODO Introduce cross entropy
-weight = torch.ones(len(output_vocab))
-pad = output_vocab.stoi[tgt.pad_token]
-loss = Perplexity(weight, pad)
-if torch.cuda.is_available():
-    loss.cuda()
-
-#################################################################################
-# Evaluate model on test set
-
-evaluator = Evaluator(loss=loss, batch_size=opt.batch_size)
-loss, accuracy, seq_accuracy = evaluator.evaluate(seq2seq, test)
-
-print("Loss: %f, Word accuracy: %f, Sequence accuracy: %f" % (loss, accuracy, seq_accuracy))
-
+for line in lines:
+    input,exp = line.split("\t")
+    output = predictor.predict(input)
+    print ("Output:", " ".join(output), "\nExpected:", exp)
--- dingen.sh
+++ dingen.sh
@@ -2,29 +2,32 @@
 
 # Start training
 #echo "Train model on babi data"
-#python3 train_model.py --train "data/CLEANED-BABI/babi-line/task1-trn.txt" --output_dir "model-line" --print_every 50 --embedding_size 128 --hidden_size 128 --epoch 6 --teacher_forcing .5 #--attention
-# --dev "data/CLEANED-SCAN/length_split/tasks_test_length.txt"
+#python3 train_model.py --train "data/CLEANED-BABI/babi-dialog/task1-trn.txt" --dev "data/CLEANED-BABI/babi-dialog/task1-dev.txt" --output_dir "model-dialog" --print_every 200 --embedding_size 128 --hidden_size 128 --epoch 50 --teacher_forcing .5 #--attention
 
-# Start training
+## Start training
 #echo "Train model on babi plus data"
-#python3 train_model.py --train "data/CLEANED-BABI/babi+line/task1-trn.txt" --output_dir "model-plus-line" --print_every 50 --embedding_size 128 --hidden_size 128 --epoch 6 --teacher_forcing .5 #--attention
-# --dev "data/CLEANED-SCAN/length_split/tasks_test_length.txt"
+#python3 train_model.py --train "data/CLEANED-BABI/babi+dialog/task1-trn.txt" --dev "data/CLEANED-BABI/babi+dialog/task1-dev.txt" --output_dir "model-plus-dialog" --print_every 50 --embedding_size 128 --hidden_size 128 --epoch 6 --teacher_forcing .5 #--attention
 
 echo "\nEvaluate babi model on babi data"
-python3 evaluate.py --checkpoint_path "model-line/"$(ls -t "model-line/" | head -1) --test_data "data/CLEANED-BABI/babi-line/task1-tst.txt"
+python3 evaluate.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/babi-dialog/task1-tst.txt"
+python3 evaluate.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/api-only/task1-tst-dialog.txt"
+#
+#echo "\nEvaluate babi plus model on babi plus data"
+#python3 evaluate.py --checkpoint_path "model-plus-dialog/"$(ls -t "model-plus-dialog/" | head -1) --test_data "data/CLEANED-BABI/babi+dialog/task1-tst.txt"
+#python3 evaluate.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/api-only/task1-tst-plus-dialog.txt"
+#
+#echo "\nEvaluate babi model on babi plus data"
+#python3 evaluate.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/babi+dialog/task1-tst.txt"
+#python3 evaluate.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/api-only/task1-tst-plus-dialog.txt"
+#
+#echo "\nEvaluate babi plus model on babi data"
+#python3 evaluate.py --checkpoint_path "model-plus-dialog/"$(ls -t "model-plus-dialog/" | head -1) --test_data "data/CLEANED-BABI/babi-dialog/task1-tst.txt"
+#python3 evaluate.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/api-only/task1-tst-dialog.txt"
 
-echo "\nEvaluate babi plus model on babi plus data"
-python3 evaluate.py --checkpoint_path "model-plus-line/"$(ls -t "model-plus-line/" | head -1) --test_data "data/CLEANED-BABI/babi+line/task1-tst.txt"
+echo "\nPredict some dialog with babi model on babi data"
+python3 babitest.py --checkpoint_path "model-dialog/"$(ls -t "model-dialog/" | head -1) --test_data "data/CLEANED-BABI/sample-dialogs/dialog.txt"
 
-echo "\nEvaluate babi model on babi plus data"
-python3 evaluate.py --checkpoint_path "model-line/"$(ls -t "model-line/" | head -1) --test_data "data/CLEANED-BABI/babi+line/task1-tst.txt"
+#echo "\nPredict some dialog with babi plus model on babi data"
+#python3 babitest.py --checkpoint_path "model-plus-dialog/"$(ls -t "model-plus-dialog/" | head -1) --test_data "data/CLEANED-BABI/sample-dialogs/dialog.txt"
+#
 
-echo "\nEvaluate babi plus model on babi data"
-python3 evaluate.py --checkpoint_path "model-plus-line/"$(ls -t "model-plus-line/" | head -1) --test_data "data/CLEANED-BABI/babi-line/task1-tst.txt"
-
-
-git remote add upstream https://github.com/i-machine-think/machine.git
-
-git fetch upstream
-git checkout master
-git rebase upstream/master
