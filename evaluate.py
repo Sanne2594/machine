1804a83fbf6e16ed2dc33e72e6ed87687e24ff1b
Sanne Bouwmeester <sanne.wopwop@gmail.com>
2018-03-01 10:56:11 +0100

    Added visualising of attention

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,37 @@
 import os
 import argparse
 import logging
-
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
 import torch
 import torchtext
+from torch.autograd import Variable
 
 import seq2seq
 from seq2seq.loss import Perplexity
 from seq2seq.dataset import SourceField, TargetField
 from seq2seq.evaluator import Evaluator, Predictor
 from seq2seq.util.checkpoint import Checkpoint
+
+
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions, cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
 
 try:
     raw_input          # Python 2
@@ -24,7 +46,7 @@ parser.add_argument('--cuda_device', default=0, type=int, help='set cuda device 
 parser.add_argument('--max_len', type=int, help='Maximum sequence length', default=50)
 parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
 parser.add_argument('--predict', action='store_true')
-
+parser.add_argument('--attviz', action='store_true')
 
 opt = parser.parse_args()
 
@@ -106,3 +128,48 @@ if(opt.predict):
         #print("\n", input)
         print("Output:", " ".join(output), "\nExpected:", exp)
 
+if(opt.attviz):
+    #if(opt.attention):
+        if(not opt.predict):
+            path = "data/CLEANED-BABI/sample-dialogs/dialog.txt"
+            if ("+dialog" in opt.test_data):
+                path = "data/CLEANED-BABI/sample-dialogs/dialog-plus.txt"
+                print("\nPredicting for Babi plus Dialogs\n")
+            elif ("plus-dialog" in opt.test_data):
+                path = "data/CLEANED-BABI/sample-dialogs/dialog-plus.txt"
+                print("\nPredicting for Babi plus Dialogs\n")
+            elif ("-dialog" in opt.test_data):
+                path = "data/CLEANED-BABI/sample-dialogs/dialog.txt"
+                print("\nPredicting for Babi Dialogs\n")
+            else:
+                print("Couldn't find matching sample dialog")
+                print(opt.test_data)
+            f = open(path, "r")
+            lines = f.readlines()
+            f.close()
+
+        #At this point lines consists of dialog input output pairs.
+
+        #lines2 = [line for line in lines if 'api_call' in line]
+        lines2 = lines
+
+        for line in lines2:
+            input,exp = line.split("\t")
+            #output = predictor.predict(input.split())
+            src_id_seq = Variable(torch.LongTensor([input_vocab.stoi[tok] for tok in input.split()]),
+                                  volatile=True).view(1, -1)
+
+            decoder_outputs, decoder_hidden, other = seq2seq(src_id_seq, [len(input.split())])
+            length = other['length'][0]
+
+            attentions = np.empty([length,len(input.split())])
+            for oi in range(length):
+                sequence =  other["attention_score"][oi]
+                attentions[oi, :] = sequence[0][0]
+
+            tgt_id_seq = [other['sequence'][di][0].data[0] for di in range(length)]
+            tgt_seq = [output_vocab.itos[tok] for tok in tgt_id_seq]
+
+            showAttention(input.split(),tgt_seq,attentions)
+    #else:
+        #print("No attention to visualize")
