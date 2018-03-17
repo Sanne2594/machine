c6f73b7503e33151c23e92b790ec0ac51867fdf6
Sanne Bouwmeester <sanne.wopwop@gmail.com>
2018-03-17 12:32:32 +0100

    length experiments results

--- Experiments/Results-Length/midlong_short.sh
+++ Experiments/Results-Length/midlong_short.sh
@@ -12,15 +12,15 @@ NUM_EP=30 #Number of Epochs
 EMB_SIZE=128
 H_SIZE=500
 
-echo "Training on shortlong"
-MDL_LOC_PLS="model-retest-plus/"
+echo "Training on midlong"
+MDL_LOC_PLS="model-midlong/"
 python3 train_model.py --train "data/BABI_midlong/cleaned/task1-trn.txt" --dev "data/BABI_midlong/cleaned/task1-dev.txt" --output_dir $MDL_LOC_PLS --print_every 200 --max_len 75 --embedding_size $EMB_SIZE --hidden_size $H_SIZE --epoch $NUM_EP --teacher_forcing .5 --attention
 
 echo "Train-scores"
 python3 evaluate.py --checkpoint_path $MDL_LOC_PLS$(ls -t $MDL_LOC_PLS | head -1) --test_data "data/BABI_midlong/cleaned/task1-trn.txt" --max_len 75
 
-echo "shortlong on shortlong - test set"
+echo "midlong on midlong - test set"
 python3 evaluate.py --checkpoint_path $MDL_LOC_PLS$(ls -t $MDL_LOC_PLS | head -1) --test_data "data/BABI_midlong/cleaned/task1-tst.txt" --max_len 75
 
-echo "shortlong on mid - test set"
+echo "midlong on short - test set"
 python3 evaluate.py --checkpoint_path $MDL_LOC_PLS$(ls -t $MDL_LOC_PLS | head -1) --test_data "data/BABI_short/cleaned/task1-tst.txt" --max_len 75
--- Experiments/Results-Length/shortmid-long.sh
+++ Experiments/Results-Length/shortmid-long.sh
@@ -12,15 +12,15 @@ NUM_EP=30 #Number of Epochs
 EMB_SIZE=128
 H_SIZE=500
 
-echo "Training on shortlong"
-MDL_LOC_PLS="model-retest-plus/"
+echo "Training on shortmid"
+MDL_LOC_PLS="model-shortmid/"
 python3 train_model.py --train "data/BABI_shortmid/cleaned/task1-trn.txt" --dev "data/BABI_shortmid/cleaned/task1-dev.txt" --output_dir $MDL_LOC_PLS --print_every 200 --max_len 75 --embedding_size $EMB_SIZE --hidden_size $H_SIZE --epoch $NUM_EP --teacher_forcing .5 --attention
 
 echo "Train-scores"
 python3 evaluate.py --checkpoint_path $MDL_LOC_PLS$(ls -t $MDL_LOC_PLS | head -1) --test_data "data/BABI_shortmid/cleaned/task1-trn.txt" --max_len 75
 
-echo "shortlong on shortlong - test set"
+echo "shortmid on shortmid - test set"
 python3 evaluate.py --checkpoint_path $MDL_LOC_PLS$(ls -t $MDL_LOC_PLS | head -1) --test_data "data/BABI_shortmid/cleaned/task1-tst.txt" --max_len 75
 
-echo "shortlong on mid - test set"
+echo "shortmid on long - test set"
 python3 evaluate.py --checkpoint_path $MDL_LOC_PLS$(ls -t $MDL_LOC_PLS | head -1) --test_data "data/BABI_long/cleaned/task1-tst.txt" --max_len 75
